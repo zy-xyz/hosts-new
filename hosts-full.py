7c24f2a39ee2501387acd343e1234459dadd393b
zy-xyz <105055851+zy-xyz@users.noreply.github.com>
2025-08-25 16:46:39 +0800

    init hostss fully
    too huge and unuseful to use

--- hosts-full.py
+++ hosts-full.py
@@ -7,7 +7,7 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 links = [
-        "https://git.acezy.top/https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
+        "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/adservers.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/allowlist.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/allowlist_stealth.txt",
@@ -17,9 +17,12 @@ links = [
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/general_elemhide.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/general_url.txt",
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/refs/heads/master/BaseFilter/sections/general_elemhide.txt",
-        "https://git.acezy.top/https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
+
+        "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
+    "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/oisd_big.txt",
     "https://easylist-downloads.adblockplus.org/easylist.txt",
     "https://easylist-downloads.adblockplus.org/easylistchina.txt",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
     "https://easylist-downloads.adblockplus.org/easyprivacy.txt",
     "https://perflyst.github.io/PiHoleBlocklist/SmartTV-AGH.txt",
     "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/oisd_big.txt",
@@ -27,22 +30,24 @@ links = [
         "https://easylist-downloads.adblockplus.org/easylist.txt",
   	"https://easylist-downloads.adblockplus.org/easylistchina.txt",
    	"https://easylist-downloads.adblockplus.org/easyprivacy.txt",
-  	"https://git.acezy.top/https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
-        "https://git.acezy.top/https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_small.txt",
-        "https://git.acezy.top/https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
-        "https://git.acezy.top/https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
-        "https://git.acezy.top/https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
+  	"https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
+        "https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_small.txt",
+        "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+        "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
+        "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
 	"https://easylist-downloads.adblockplus.org/easylist.txt",
 	"https://filters.adtidy.org/windows/filters/224_optimized.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
+	"https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
 	"https://easylist.to/easylist/fanboy-annoyance.txt",
 	"https://easylist.to/easylist/easyprivacy.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://easylist.to/easylist/fanboy-social.txt",
+	"https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt",
 	"https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-agh.txt",
  	"https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-hosts-online.txt",
- 	"https://git.acezy.top/https://raw.githubusercontent.com/lingeringsound/10007/main/all",
-	"https://git.acezy.top/https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
+ 	"https://raw.githubusercontent.com/lingeringsound/10007/main/all",
+	"https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
+	"https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt",
  	"https://easylist-downloads.adblockplus.org/antiadblockfilters.txt",
 	"https://easylist-downloads.adblockplus.org/easylistchina.txt",
 	"https://filters.adtidy.org/android/filters/15_optimized.txt",
@@ -50,44 +55,44 @@ links = [
 	"https://filters.adtidy.org/extension/ublock/filters/11.txt",
 	"https://github.com/Potterli20/file/releases/download/github-hosts/Accelerate-Hosts.txt",
 	"https://anti-ad.net/easylist.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/pboymt/Steam520/main/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/adguard",
+	"https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts",
+	"https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt",
+	"https://raw.githubusercontent.com/pboymt/Steam520/main/hosts",
+	"https://raw.githubusercontent.com/rentianyu/Ad-set-hosts/master/adguard",
 	"https://lingeringsound.github.io/adblock_auto/Rules/adblock_auto.txt",
 	"https://lingeringsound.github.io/adblock_auto/Rules/adblock_auto_lite.txt",
 	"https://raw.fgit.ml/lingeringsound/adblock/master/Toutiao_block.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/jianboy/github-host/master/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/zsakvo/AdGuard-Custom-Rule/master/rule/zhihu-strict.txt",
-	"https://small.oisd.nl/",
-	"https://git.acezy.top/https://raw.githubusercontent.com/francis-zhao/quarklist/master/dist/quarklist.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_adblocker",
-	"https://git.acezy.top/https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt",
+	"https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt",
+	"https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt",
+	"https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
+	"https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt",
+	"https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt",
+	"https://raw.githubusercontent.com/banbendalao/ADgk/master/kill-baidu-ad.txt",
+	"https://raw.githubusercontent.com/jianboy/github-host/master/hosts",
+	"https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt",
+	"https://raw.githubusercontent.com/zsakvo/AdGuard-Custom-Rule/master/rule/zhihu-strict.txt",
+	"https://big.oisd.nl/",
+	"https://raw.githubusercontent.com/francis-zhao/quarklist/master/dist/quarklist.txt",
+	"https://raw.githubusercontent.com/neodevpro/neodevhost/master/lite_adblocker",
+	"https://raw.githubusercontent.com/Cats-Team/AdRules/main/adblock_lite.txt",
 	"https://gitlab.com/ineo6/hosts/-/raw/master/next-hosts",
 	"https://raw.hellogithub.com/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+	"https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
+	"https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
+	"https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
 	"https://cdn.jsdelivr.net/gh/sbwml/halflife-list@master/ad.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/596546419/ad-filters-subscriber/main/rule/local-rule.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/adblock.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/allow.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/zsokami/ACL4SSR/main/hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/Clov614/SteamHostSync/main/Hosts",
+	"https://raw.githubusercontent.com/596546419/ad-filters-subscriber/main/rule/local-rule.txt",
+	"https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/adblock.txt",
+	"https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/allow.txt",
+	"https://raw.githubusercontent.com/zsokami/ACL4SSR/main/hosts",
+	"https://raw.githubusercontent.com/Clov614/SteamHostSync/main/Hosts",
 	"https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts",
-	"https://git.acezy.top/https://raw.githubusercontent.com/JohyC/Hosts/refs/heads/main/hosts.txt",
-	"https://git.acezy.top/https://raw.githubusercontent.com/geoisam/FuckScripts/main/adfuck.txt"    
+	"https://raw.githubusercontent.com/JohyC/Hosts/refs/heads/main/hosts.txt",
+	"https://raw.githubusercontent.com/geoisam/FuckScripts/main/adfuck.txt"    
 ]
 dead_hosts = [
-    "https://git.acezy.top/https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt",
-    "https://git.acezy.top/https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.dead.txt"
+    "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt",
+    "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.dead.txt"
 ]
 
 CACHE = "cache"
@@ -207,11 +212,11 @@ def build():
     hosts, dead = load()
     acc_hosts, easylist, adblock = parallel_classify(hosts, dead)
 
-    with open("accelerate.txt", "w", encoding='utf-8') as f:
+    with open("accelerate_full.txt", "w", encoding='utf-8') as f:
         f.write("\n".join(acc_hosts))
-    with open("easylist.txt", "w", encoding='utf-8') as f:
+    with open("easylist_full.txt", "w", encoding='utf-8') as f:
         f.write("\n".join(easylist))
-    with open("adblock.txt", "w", encoding='utf-8') as f:
+    with open("adblock_full.txt", "w", encoding='utf-8') as f:
         f.write("\n".join(adblock))
 
 if __name__ == "__main__":
