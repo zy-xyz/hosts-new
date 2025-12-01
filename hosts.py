7b6450822493ad5a998bda3ce2b2523507f481c1
zy-xyz <105055851+zy-xyz@users.noreply.github.com>
2025-12-01 14:46:19 +0800

    Remove adblock rules URL from hosts
    Removed a URL for adblock rules from the hosts list.

--- hosts.py
+++ hosts.py
@@ -45,7 +45,6 @@ links = [
     "https://raw.githubusercontent.com/entr0pia/fcm-hosts/fcm/fcm-hosts",
     "https://raw.githubusercontent.com/JohyC/Hosts/refs/heads/main/hosts.txt",
     "https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/dns.txt",
-    "https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/adblock.txt",
     "https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/allow.txt",
     "https://raw.githubusercontent.com/zy-xyz/hosts-new/refs/heads/main/diy.txt",
     "https://github-hosts.tinsfox.com/hosts",
